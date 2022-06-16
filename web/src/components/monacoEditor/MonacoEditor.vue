49b6c646cf4b5dcc1ea20665b3bfe869825c36d0
yutengjing <ytj2713151713@gmail.com>
2022-06-17 02:52:48 +0800

    feat: integrate monoca editor

---
+++ web/src/components/monacoEditor/MonacoEditor.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import './userWorker';
+
+const editorContainer = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+    monaco.editor.create(editorContainer.value!, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+        language: 'typescript',
+    });
+});
+
+defineExpose({
+    editorContainer,
+});
+</script>
+
+<template>
+    <div class="editor" ref="editorContainer"></div>
+</template>
+
+<style lang="scss"></style>
--- web/src/pages/layer/Layer.vue
+++ web/src/pages/layer/Layer.vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+import MonacoEditor from '../../components/monacoEditor/MonacoEditor.vue';
+</script>
+
 <template>
-    <div class="page layer-page">这里将展示图层信息</div>
+    <div class="page layer-page">
+        <MonacoEditor />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style lang="scss"></style>
+<style lang="scss">
+.layer-page {
+    .editor {
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
